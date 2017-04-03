e841599e60f04cb793e0444b6c57bebd404c76ed
Tayfun Turgut <tcfturgut@gmail.com>
2017-04-03 20:11:36 +0300

    V1.1

--- sketch.js
+++ sketch.js
@@ -1,15 +1,26 @@
-var transX;
-var transY;
 var snake;
+var scl = 20;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    transX = canvas.width * 0.5;
-    transY = canvas.height * 0.5;
-    background(51);
     snake = new Snake();
+    
 }
 
 function draw() {
-  
+    background(51);
+    snake.Update();
+    snake.Render();
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    snake.Dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.Dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.Dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    snake.Dir(-1, 0);
+  }
 }
--- snake.js
+++ snake.js
@@ -1,6 +1,26 @@
 function Snake() {
-    this.x = 0;
-    this.y = 0;
+    this.x = width/2;
+    this.y = height/2;
     this.xSpeed = 0;
     this.ySpeed = 0;
+
+    this.Render = function() {
+        fill(255);
+        rect(this.x, this.y, scl, scl);
+    }
+
+    this.Update = function() {
+        this.x = constrain(this.x, 0, width - scl);
+        this.y = constrain(this.y, 0, height - scl);
+    }
+
+    this.Move = function() {
+        this.x = this.x + this.xSpeed * scl;
+        this.y = this.y + this.ySpeed * scl;
+    }
+
+    this.Dir = function(x, y) {
+        this.xSpeed = x;
+        this.ySpeed = y;
+    }
 }
