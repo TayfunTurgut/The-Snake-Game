ccc28609042396e6b6a254c8b76a8c54cf65022e
Tayfun Turgut <tcfturgut@gmail.com>
2017-04-05 01:25:15 +0300

    V2.0
    Finished basic gameplay (complete coding overhaul comparing to the
    coding train), + added:
    - High score system
    - Movement modifications
    - Death reset modifications
    - DOM elements
    - Speed up upon eating food

--- sketch.js
+++ sketch.js
@@ -1,31 +1,77 @@
 var snake;
-var scl = 20;
+var scl = 10;
 var tick;
+var food;
+var p;
+var s;
+var tickSpeed = 100;
+var keyTimer = 10;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    snake = new Snake();
-    Tick(snake.Move, 500);
-}
-
-function draw() {
-    background(51);
-    snake.Update();
-    snake.Render();
-}
-
-function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    snake.Dir(0, -1);
-  } else if (keyCode === DOWN_ARROW) {
-    snake.Dir(0, 1);
-  } else if (keyCode === RIGHT_ARROW) {
-    snake.Dir(1, 0);
-  } else if (keyCode === LEFT_ARROW) {
-    snake.Dir(-1, 0);
+  createCanvas(600, 600);
+  snake = new Snake();
+  spawnFood();
+  Tick(tickSpeed);
+  p = document.getElementById("score");
+  s = document.getElementById("highScore");
+  if (localStorage.highScore == null) {
+    localStorage._highScore = "0";
   }
 }
 
-function Tick(func, t) {
-	tick = setInterval(func, t);
+function draw() {
+  background(51);
+  snake.constrain();
+  snake.render();
+  snake.death();
+
+  if (keyTimer > 0) {
+    keyTimer--;
+  }
+
+  if (snake.eat(food)) {
+    spawnFood();
+    speedUp();
+  }
+
+  fill(255, 0, 100)
+  rect(food.x, food.y, scl, scl);
+
+  p.innerHTML = "Your score is: " + snake.total.toString();
+  s.innerHTML = "Your high score is: " + localStorage._highScore + "!";
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW && keyTimer == 0 && snake.ySpeed != 1) {
+    snake.dir(0, -1);
+    keyTimer = 6;
+  } else if (keyCode === DOWN_ARROW && keyTimer == 0 && snake.ySpeed != -1) {
+    snake.dir(0, 1);
+    keyTimer = 6;
+  } else if (keyCode === RIGHT_ARROW && keyTimer == 0 && snake.xSpeed != -1) {
+    snake.dir(1, 0);
+    keyTimer = 6;
+  } else if (keyCode === LEFT_ARROW && keyTimer == 0 && snake.xSpeed != 1) {
+    snake.dir(-1, 0);
+    keyTimer = 6;
+  }
+}
+
+function Tick(t) {
+  tick = setInterval(function () {
+    snake.move()
+  }, t);
+}
+
+function spawnFood() {
+  var cols = floor(width / scl);
+  var rows = floor(height / scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
+function speedUp() {
+  clearInterval(tick);
+  tickSpeed -= 2;
+  Tick(tickSpeed);
 }
--- snake.js
+++ snake.js
@@ -1,26 +1,76 @@
 function Snake() {
-    this.x = width/2;
-    this.y = height/2;
+    this.x = width / 2;
+    this.y = height / 2;
     this.xSpeed = 0;
     this.ySpeed = 0;
+    this.total = 0;
+    this.tail = [];
 
-    this.Render = function() {
+    this.render = function () {
         fill(255);
         rect(this.x, this.y, scl, scl);
+
+        if (this.tail.length >= 1) {
+            for (var i = 0; i < this.tail.length; i++) {
+                rect(this.tail[i].x, this.tail[i].y, scl, scl);
+            }
+        }
     }
 
-    this.Update = function() {
+    this.constrain = function () {
         this.x = constrain(this.x, 0, width - scl);
         this.y = constrain(this.y, 0, height - scl);
     }
 
-    this.Move = function() {
+    this.move = function () {
+        if (this.total >= 2) {
+            for (var i = this.total - 1; i > 0; i--) {
+                this.tail[i] = this.tail[i - 1]
+            }
+        }
+        if (this.total >= 1) {
+            this.tail[0] = createVector(this.x, this.y);
+        }
+
         this.x = this.x + this.xSpeed * scl;
         this.y = this.y + this.ySpeed * scl;
     }
 
-    this.Dir = function(x, y) {
+    this.dir = function (x, y) {
         this.xSpeed = x;
         this.ySpeed = y;
+    }
+
+    this.eat = function (pos) {
+        var d = dist(this.x, this.y, pos.x, pos.y);
+        if (d < 1) {
+            this.total++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    this.death = function () {
+        for (var i = 0; i < this.tail.length; i++) {
+            var pos = this.tail[i];
+            var d = dist(this.x, this.y, pos.x, pos.y);
+            if (d < 1) {
+                if (parseInt(localStorage._highScore) < this.total) {
+                    localStorage._highScore = this.total.toString();
+                }
+                this.reset();
+            }
+        }
+    }
+
+    this.reset = function () {
+        this.total = 0;
+        this.tail = [];
+        this.x = width / 2;
+        this.y = height / 2;
+        this.xSpeed = 0;
+        this.ySpeed = 0;
+        spawnFood();
     }
 }
